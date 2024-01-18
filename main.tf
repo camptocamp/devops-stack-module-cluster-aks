a02b74d7fb6244d7a58d1ad9d7fcc6c207a76364
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2024-01-18 17:45:39 +0100

    refactor: put every resource on main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+data "azurerm_dns_zone" "this" {
+  name                = var.base_domain
+  resource_group_name = var.dns_zone_resource_group_name
+}
+
+resource "azurerm_dns_cname_record" "this" {
+  name                = format("*.apps.%s", var.cluster_name)
+  zone_name           = data.azurerm_dns_zone.this.name
+  resource_group_name = var.dns_zone_resource_group_name
+  ttl                 = 300
+  record              = format("%s-%s.%s.cloudapp.azure.com.", var.cluster_name, replace(data.azurerm_dns_zone.this.name, ".", "-"), resource.azurerm_resource_group.this.location)
+}
+
 resource "azurerm_subnet" "this" {
   name                 = "${var.cluster_name}-snet"
   resource_group_name  = var.resource_group_name
